b5c67f99f66b99b7e62463ff484d179168eeae72
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2024-05-27 09:48:19 -0500

    create function getComputerchoice() to get randomly generated selection of rock, paper, or scissors

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+    options = ['rock', 'paper', 'scissors'];
+    indx = Math.floor(Math.random() * 3);
+
+    return options[indx];
+}
