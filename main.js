5249322e62b2ea81dfdb6bfc6bc31dcfb493dfe8
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2024-05-27 09:53:50 -0500

    create getHumanChoice function to prompt user for their move and return the value

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 console.log(getComputerChoice());
-
+console.log(getHumanChoice());
 function getComputerChoice() {
-    options = ['rock', 'paper', 'scissors'];
-    indx = Math.floor(Math.random() * 3);
+    let options = ['rock', 'paper', 'scissors'];
+    let indx = Math.floor(Math.random() * 3);
 
     return options[indx];
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Your selection: ");
+
+    return humanChoice;
 }
