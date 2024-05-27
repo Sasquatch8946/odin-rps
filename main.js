24c2fca9acd60392fbeef016d43097ceb1c9a551
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2024-05-27 10:08:09 -0500

    create playRound function to play a round

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+
+playRound(humanChoice, computerChoice);
+
 function getComputerChoice() {
     let options = ['rock', 'paper', 'scissors'];
     let indx = Math.floor(Math.random() * 3);
@@ -9,6 +12,35 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt("Your selection: ");
+    const options = ['rock', 'paper', 'scissors'];
+    if (options.indexOf(humanChoice.toLowerCase()) < 0) {
+        throw "Invalid selection. Please enter 'rock,' 'paper', or 'scissors.'"
+    } else {
+        return humanChoice;
+    }
+}
 
-    return humanChoice;
+function playRound(humanChoice, computerChoice) {
+    let hChoice = humanChoice.toLowerCase();
+    let cChoice = computerChoice.toLowerCase();
+
+    if (hChoice === 'rock' && cChoice === 'rock') {
+        console.log("It's a tie. Both players chose 'rock.'");
+    } else if (hChoice === 'rock' && cChoice === 'paper') {
+        console.log("You lose. Paper beats rock.");
+    } else if (hChoice === 'rock' && cChoice === 'scissors') {
+        console.log("You win! Rock beats scissors.");
+    } else if (hChoice === 'paper' && cChoice === 'paper') {
+        console.log("It's a tie. Both players chose 'paper.'");
+    } else if (hChoice === 'paper' && cChoice === 'scissors') {
+        console.log("You lose. Scissors beats paper.");
+    } else if (hChoice === 'paper' && cChoice === 'rock') {
+        console.log("You win! Paper beats rock.");
+    } else if (hChoice === 'scissors' && cChoice === 'scissors') {
+        console.log("It's a tie. Both players chose 'scissors.'");
+    } else if (hChoice === 'scissors' && cChoice === 'rock') {
+        console.log("You lose. Rock beats scissors.");
+    } else if (hChoice === 'scissors' && cChoice === 'paper') {
+        console.log("You win! Scissors beats paper.");
+    }
 }
