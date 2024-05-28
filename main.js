475a578e82db0660cae91684e7692c0c638ed568
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2024-05-27 23:30:48 -0500

    add event listeners to my three buttons, create results div to display results, make it so winner is announced after 5 rounds

--- main.js
+++ main.js
@@ -16,33 +16,33 @@ function getHumanChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice, resultsDiv) {
     let hChoice = humanChoice.toLowerCase();
     let cChoice = computerChoice.toLowerCase();
 
     if (hChoice === 'rock' && cChoice === 'rock') {
-        console.log("It's a tie. Both players chose 'rock.'");
+        resultsDiv.innerText =  "It's a tie. Both players chose 'rock.'";
     } else if (hChoice === 'rock' && cChoice === 'paper') {
-        console.log("You lose. Paper beats rock.");
+        resultsDiv.innerText =  "You lose. Paper beats rock.";
         return "computer";
     } else if (hChoice === 'rock' && cChoice === 'scissors') {
-        console.log("You win! Rock beats scissors.");
+        resultsDiv.innerText =  "You win! Rock beats scissors.";
         return "human";
     } else if (hChoice === 'paper' && cChoice === 'paper') {
-        console.log("It's a tie. Both players chose 'paper.'");
+        resultsDiv.innerText =  "It's a tie. Both players chose 'paper.'";
     } else if (hChoice === 'paper' && cChoice === 'scissors') {
-        console.log("You lose. Scissors beats paper.");
+        resultsDiv.innerText =  "You lose. Scissors beats paper.";
         return "computer";
     } else if (hChoice === 'paper' && cChoice === 'rock') {
-        console.log("You win! Paper beats rock.");
+        resultsDiv.innerText =  "You win! Paper beats rock.";
         return "human";
     } else if (hChoice === 'scissors' && cChoice === 'scissors') {
-        console.log("It's a tie. Both players chose 'scissors.'");
+        resultsDiv.innerText =  "It's a tie. Both players chose 'scissors.'";
     } else if (hChoice === 'scissors' && cChoice === 'rock') {
-        console.log("You lose. Rock beats scissors.");
+        resultsDiv.innerText =  "You lose. Rock beats scissors.";
         return "computer";
     } else if (hChoice === 'scissors' && cChoice === 'paper') {
-        console.log("You win! Scissors beats paper.");
+        resultsDiv.innerText =  "You win! Scissors beats paper.";
         return "human";
     }
 }
@@ -50,29 +50,57 @@ function playRound(humanChoice, computerChoice) {
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    // register event listeners for buttons
 
-    for (let i = 0; i < 6; i++) {
-        const computerChoice = getComputerChoice();
-        const humanChoice = getHumanChoice();
-        let winner = playRound(humanChoice, computerChoice);
-        if (winner === 'human') {
-            humanScore += 1;
-        } else if (winner === 'computer') {
-            computerScore += 1;
-        }
-        console.log(`Your score: ${humanScore}`);
-        console.log(`COM's score: ${computerScore}`);
+    const rockButton = document.querySelector("#rock");
+    rockButton.addEventListener("click", playOnClick);
+
+    const paperButton = document.querySelector("#paper");
+    paperButton.addEventListener("click", playOnClick);
+
+    const scissorsButton = document.querySelector("#scissors");
+    scissorsButton.addEventListener("click", playOnClick);
+    
+    const computerChoice = getComputerChoice();
+
+    let winner = playRound(humanChoice, computerChoice);
+    if (winner === 'human') {
+        humanScore += 1;
+    } else if (winner === 'computer') {
+        computerScore += 1;
     }
-
-    if (humanScore > computerScore) {
-        console.log("You've won!")
-    } else if (computerScore > humanScore) {
-        console.log("You lost!");
-    } else if (computerScore === humanScore) {
-        console.log("It was a tie!")
-    }
-
-    console.log(`Final score: ${humanScore} to ${computerScore}.`)
+    resultsDiv.innerText =  (`Your score: ${humanScore}`);
+    resultsDiv.innerText =  (`COM's score: ${computerScore}`);
 }
 
-playGame();
+function playOnClick(event) {
+    const humanChoice = event.target.id;
+    const computerChoice = getComputerChoice();
+    const resultsDiv = document.querySelector("div.results");
+    let winner = playRound(humanChoice, computerChoice, resultsDiv);
+    if (winner === 'human') {
+        humanScore += 1;
+    } else if (winner === 'computer') {
+        computerScore += 1;
+    }
+    if (humanScore === 5) {
+        resultsDiv.innerText += `\nYou won the game ${humanScore} to ${computerScore}.`
+    } else if (computerScore === 5) {
+        resultsDiv.innerText += `You lost the game ${humanScore} to ${computerScore}.`
+    } else {
+        resultsDiv.innerText += `\nYour score: ${humanScore}\nCOM's score: ${computerScore}`
+    }
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const rockButton = document.querySelector("#rock");
+rockButton.addEventListener("click", playOnClick);
+
+const paperButton = document.querySelector("#paper");
+paperButton.addEventListener("click", playOnClick);
+
+const scissorsButton = document.querySelector("#scissors");
+scissorsButton.addEventListener("click", playOnClick);
