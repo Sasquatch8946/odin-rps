5861f8e54b7ba8a6029d878c835c0b9cf306f6ad
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2024-05-27 22:37:00 -0500

    Finish playGame() function to create a game of 5 rounds; add content to README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # odin-rps
+
+I did this as part of The Odin project's Foundations course. 
+
+A simple version of "Rock, paper, scissors" played in the web browser. Results of each round as well as the final results of the game are viewable in the console. 
--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-const computerChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
-
-playRound(humanChoice, computerChoice);
 
 function getComputerChoice() {
     let options = ['rock', 'paper', 'scissors'];
@@ -28,19 +24,55 @@ function playRound(humanChoice, computerChoice) {
         console.log("It's a tie. Both players chose 'rock.'");
     } else if (hChoice === 'rock' && cChoice === 'paper') {
         console.log("You lose. Paper beats rock.");
+        return "computer";
     } else if (hChoice === 'rock' && cChoice === 'scissors') {
         console.log("You win! Rock beats scissors.");
+        return "human";
     } else if (hChoice === 'paper' && cChoice === 'paper') {
         console.log("It's a tie. Both players chose 'paper.'");
     } else if (hChoice === 'paper' && cChoice === 'scissors') {
         console.log("You lose. Scissors beats paper.");
+        return "computer";
     } else if (hChoice === 'paper' && cChoice === 'rock') {
         console.log("You win! Paper beats rock.");
+        return "human";
     } else if (hChoice === 'scissors' && cChoice === 'scissors') {
         console.log("It's a tie. Both players chose 'scissors.'");
     } else if (hChoice === 'scissors' && cChoice === 'rock') {
         console.log("You lose. Rock beats scissors.");
+        return "computer";
     } else if (hChoice === 'scissors' && cChoice === 'paper') {
         console.log("You win! Scissors beats paper.");
+        return "human";
     }
 }
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 6; i++) {
+        const computerChoice = getComputerChoice();
+        const humanChoice = getHumanChoice();
+        let winner = playRound(humanChoice, computerChoice);
+        if (winner === 'human') {
+            humanScore += 1;
+        } else if (winner === 'computer') {
+            computerScore += 1;
+        }
+        console.log(`Your score: ${humanScore}`);
+        console.log(`COM's score: ${computerScore}`);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You've won!")
+    } else if (computerScore > humanScore) {
+        console.log("You lost!");
+    } else if (computerScore === humanScore) {
+        console.log("It was a tie!")
+    }
+
+    console.log(`Final score: ${humanScore} to ${computerScore}.`)
+}
+
+playGame();
